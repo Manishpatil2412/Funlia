241aa8dc6e581a981839f0f734b921c27c30b4fb
Bhaskar Nishad <62388614+fromBhaskar@users.noreply.github.com>
2023-03-03 10:57:01 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/funlia/MainActivity.kt
+++ app/src/main/java/com/example/funlia/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this,"Tarang",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,"Patil",Toast.LENGTH_SHORT).show()
     }
 }
